71039e39104019ebe3837a7ca8d49b3f2c590411
BrandonELin <theb.lin01@gmail.com>
2022-10-18 13:41:10 -0400

    App test

--- pet-api/src/App.js
+++ pet-api/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+
+let init = false;
 
 function App() {
+  const apiKey = process.env.REACT_APP_API;
+
+  let [pet, setPet] = useState(null);
+  let [animal, setAnimals] = useState([]);
+
+  let navigate = useNavigate();
+
+  const getAnimals = async() => {
+
+    try {
+      const animalList = await fetch(`https://api.petfinder.com/v2/animals?status=adoptable`);
+      const animals = await animalList.json();
+      console.log(animals)
+      setAnimals(animals)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //  async / await
+  const getPet = async (petName) => {
+    try {
+      const response = await fetch(`https://api.petfinder.com/v2/animals/${petName}`);
+      const data = await response.json();
+      console.log(data);
+      setPet(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
