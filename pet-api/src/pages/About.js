7a824d577884affc8f3211a90d2c46c85f9a54fd
Jessica Milewski <jlm323@pitt.edu>
2022-10-18 14:20:18 -0400

    about page text

--- pet-api/src/pages/About.js
+++ pet-api/src/pages/About.js
@@ -2,6 +2,8 @@ export default function About() {
     return (
         <div>
             <h2>About Our Agency</h2>
+            <p>One rainy evening, our founder Brandon happened upon a small kitten huddled under a cardboard box. He quickly grabbed the kitten and rushed into the warmth of his home. After inspection, Brandon saw the kitten was fine. He wrapped the kitten in a blanket and promised himself that from that day forth, he would save as many of the homeless, rainsoaked animals as he could.</p>
+            <p>In collaboration with co-founders Kristy, Mamdouh, and Jessica, our pet rescue and adoption agency was created, with the goal of getting all animals their forever homes.</p>
         </div>
     )
 }
